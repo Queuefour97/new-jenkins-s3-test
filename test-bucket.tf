6807e10c845e2ef20a0c9196fe772de113d049bf
Queuefour97 Jorune <107631310+Queuefour97@users.noreply.github.com>
2026-03-22 23:36:44 -0400

    test webhook

--- test-bucket.tf
+++ test-bucket.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "terraform-state-aaronmcd"                 # Name of the S3 bucket
-    key     = "jenkins-test-031726.tfstate"        # The name of the state file in the bucket
-    region  = "us-east-2"                          # Use a variable for the region
+    bucket  = "backend-bucket-830"                 # Name of the S3 bucket
+    key     = "jenkins-test-032226.tfstate"        # The name of the state file in the bucket
+    region  = "us-east-1"                          # Use a variable for the region
     encrypt = true                                 # Enable server-side encryption (optional but recommended)
   } 
 }
