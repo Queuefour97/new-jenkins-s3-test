6e2f2542a27dd14dd3d2eb83a233426af0efa2f4
Queuefour97 Jorune <107631310+Queuefour97@users.noreply.github.com>
2026-03-28 15:37:49 -0400

    Jorune's  Jenkins  Test

--- test-bucket.tf
+++ test-bucket.tf
@@ -7,23 +7,33 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "backend-bucket-830"                 # Name of the S3 bucket
-    key     = "jenkins-test-032326.tfstate"        # The name of the state file in the bucket
-    region  = "us-east-1"                          # Use a variable for the region
-    encrypt = true                                 # Enable server-side encryption (optional but recommended)
-  } 
+    bucket  = "backend-bucket-830"          # Name of the S3 bucket
+    key     = "jenkins-test-032326.tfstate" # The name of the state file in the bucket
+    region  = "us-east-1"                   # Use a variable for the region
+    encrypt = true                          # Enable server-side encryption (optional but recommended)
+  }
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "frontend" {
   bucket_prefix = "jenkins-bucket-"
   force_destroy = true
-  
+
 
   tags = {
     Name = "Jenkins Bucket"
   }
+}
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.frontend.bucket
+  key    = "images/forS3bucket03262026.png"
+  source = "./forS3bucket03262026.png"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("./forS3bucket03262026.png")
 }
