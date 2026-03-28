d16a6c9b166d097c0d4cd099eb62470899d8d1ff
Queuefour97 Jorune <107631310+Queuefour97@users.noreply.github.com>
2026-03-28 17:53:35 -0400

    Jorune's  Jenkins  Test

---
+++ armegeddon_repo_link.md
@@ -0,0 +1,2 @@
+## Below is the link for the armegeddon repo for Jorune Simpkins
+https://github.com/Queuefour97/student_deliverables_Armageddon
--- test-bucket.tf
+++ test-bucket.tf
@@ -37,3 +37,53 @@ resource "aws_s3_object" "object" {
   # etag = "${md5(file("path/to/file"))}"
   etag = filemd5("./forS3bucket03262026.png")
 }
+resource "aws_s3_object" "object2" {
+  bucket = aws_s3_bucket.frontend.bucket
+  key    = "images/ec2jenkinsbucket.png"
+  source = "./ec2jenkinsbucket.png"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("./ec2jenkinsbucket.png")
+}
+resource "aws_s3_object" "object3" {
+  bucket = aws_s3_bucket.frontend.bucket
+  key    = "images/jenkinspipeline1.png"
+  source = "./jenkinspipeline1.png"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("./jenkinspipeline1.png")
+}
+resource "aws_s3_object" "object4" {
+  bucket = aws_s3_bucket.frontend.bucket
+  key    = "images/s3bucketjenkins.png"
+  source = "./s3bucketjenkins.png"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("./s3bucketjenkins.png")
+}
+resource "aws_s3_object" "object5" {
+  bucket = aws_s3_bucket.frontend.bucket
+  key    = "images/webhook_proof.png"
+  source = "./webhook_proof.png"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("./webhook_proof.png")
+}
+resource "aws_s3_object" "object6" {
+  bucket = aws_s3_bucket.frontend.bucket
+  key    = "images/webhook_proof2.png"
+  source = "./webhook_proof2.png"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("./webhook_proof2.png")
+}
