1e8d909e04c2cf47b39ae7de52549a5c5dbcd33f
Queuefour97 Jorune <107631310+Queuefour97@users.noreply.github.com>
2026-03-29 14:07:25 -0400

    Jorune's  Jenkins  Test

--- armageddon_repo_link.md
+++ armageddon_repo_link.md
@@ -1,3 +1,3 @@
 ## Below is the link for the armegeddon non-forked repo for: Jorune Simpkins
-## Date:03282026
+## Date:03292026
 https://github.com/Queuefour97/student_deliverables_Armageddon
--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ terraform {
     }
   }
 
+
+
   backend "s3" {
     bucket  = "backend-bucket-830"          # Name of the S3 bucket
     key     = "jenkins-test-032326.tfstate" # The name of the state file in the bucket
@@ -27,10 +29,25 @@ resource "aws_s3_bucket" "frontend" {
     Name = "Jenkins Bucket"
   }
 }
+
+resource "aws_s3_bucket_website_configuration" "aws_s3_website" {
+  bucket = aws_s3_bucket.frontend.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+
 resource "aws_s3_object" "object" {
-  bucket = aws_s3_bucket.frontend.bucket
-  key    = "images/forS3bucket03262026.png"
-  source = "./forS3bucket03262026.png"
+  bucket       = aws_s3_bucket.frontend.bucket
+  key          = "images/forS3bucket03262026.png"
+  source       = "./forS3bucket03262026.png"
+  content_type = "image/png"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
@@ -38,9 +55,10 @@ resource "aws_s3_object" "object" {
   etag = filemd5("./forS3bucket03262026.png")
 }
 resource "aws_s3_object" "object2" {
-  bucket = aws_s3_bucket.frontend.bucket
-  key    = "images/ec2jenkinsbucket.png"
-  source = "./ec2jenkinsbucket.png"
+  bucket       = aws_s3_bucket.frontend.bucket
+  key          = "images/ec2jenkinsbucket.png"
+  source       = "./ec2jenkinsbucket.png"
+  content_type = "image/png"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
@@ -48,9 +66,10 @@ resource "aws_s3_object" "object2" {
   etag = filemd5("./ec2jenkinsbucket.png")
 }
 resource "aws_s3_object" "object3" {
-  bucket = aws_s3_bucket.frontend.bucket
-  key    = "images/jenkinspipeline1.png"
-  source = "./jenkinspipeline1.png"
+  bucket       = aws_s3_bucket.frontend.bucket
+  key          = "images/jenkinspipeline1.png"
+  source       = "./jenkinspipeline1.png"
+  content_type = "image/png"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
@@ -58,9 +77,10 @@ resource "aws_s3_object" "object3" {
   etag = filemd5("./jenkinspipeline1.png")
 }
 resource "aws_s3_object" "object4" {
-  bucket = aws_s3_bucket.frontend.bucket
-  key    = "images/s3bucketinjenkins.png"
-  source = "./s3bucketinjenkins.png"
+  bucket       = aws_s3_bucket.frontend.bucket
+  key          = "images/s3bucketinjenkins.png"
+  source       = "./s3bucketinjenkins.png"
+  content_type = "image/png"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
@@ -68,9 +88,10 @@ resource "aws_s3_object" "object4" {
   etag = filemd5("./s3bucketinjenkins.png")
 }
 resource "aws_s3_object" "object5" {
-  bucket = aws_s3_bucket.frontend.bucket
-  key    = "images/webhook_proof.png"
-  source = "./webhook_proof.png"
+  bucket       = aws_s3_bucket.frontend.bucket
+  key          = "images/webhook_proof.png"
+  source       = "./webhook_proof.png"
+  content_type = "image/png"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
@@ -78,9 +99,10 @@ resource "aws_s3_object" "object5" {
   etag = filemd5("./webhook_proof.png")
 }
 resource "aws_s3_object" "object6" {
-  bucket = aws_s3_bucket.frontend.bucket
-  key    = "images/webhook_proof2.png"
-  source = "./webhook_proof2.png"
+  bucket       = aws_s3_bucket.frontend.bucket
+  key          = "images/webhook_proof2.png"
+  source       = "./webhook_proof2.png"
+  content_type = "image/png"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
@@ -89,32 +111,36 @@ resource "aws_s3_object" "object6" {
 }
 
 resource "aws_s3_object" "object7" {
-  bucket = aws_s3_bucket.frontend.bucket
-  key    = "images/armageddon_repo_screenshot_non_forked.png"
-  source = "./armageddon_repo_screenshot_non_forked.png"
+  bucket       = aws_s3_bucket.frontend.bucket
+  key          = "images/armageddon_repo_screenshot_non_forked.png"
+  source       = "./armageddon_repo_screenshot_non_forked.png"
+  content_type = "image/png"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
   etag = filemd5("./armageddon_repo_screenshot_non_forked.png")
+
 }
 
 resource "aws_s3_object" "object8" {
-  bucket = aws_s3_bucket.frontend.bucket
-  key    = "images/armageddon_repo_link.md"
-  source = "./armageddon_repo_link.md"
+  bucket       = aws_s3_bucket.frontend.bucket
+  key          = "images/armageddon_repo_link.md"
+  source       = "./armageddon_repo_link.md"
+  content_type = "text/markdown; charset=UTF-8"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
   etag = filemd5("./armageddon_repo_link.md")
-  
+
 }
 
 resource "aws_s3_object" "object9" {
-  bucket = aws_s3_bucket.frontend.bucket
-  key    = "images/lab3b_runbook.docx"
-  source = "${path.module}/lab3b_runbook.docx"
+  bucket       = aws_s3_bucket.frontend.bucket
+  key          = "images/lab3b_runbook.docx"
+  source       = "${path.module}/lab3b_runbook.docx"
+  content_type = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
